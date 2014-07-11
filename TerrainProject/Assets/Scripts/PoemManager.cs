bbd7755f381f98864921a51e53d1565d765e47b0
M. Palladino <Michael21115@yahoo.com>
2014-07-10 21:06:01 -0400

    Terrain Build Version 2.0 - Poetry Landscape First Draft

---
+++ TerrainProject/Assets/Scripts/PoemManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoemManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
